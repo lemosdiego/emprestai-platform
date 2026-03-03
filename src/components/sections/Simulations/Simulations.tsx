060c730c97ede96ea56e29d4ed9412da3ed404c6
Washington <washington.contato1327@gmail.com>
2026-03-03 10:15:50 -0300

    feat(sections)Cria seção responsavel por mostrar as simulações

---
+++ src/components/sections/Simulations/Simulations.tsx
@@ -0,0 +1,29 @@
+import { useLocation } from "react-router-dom";
+import type { SimulacaoResponse } from "../../../types/simulacao";
+
+export default function Simulations() {
+  const location = useLocation();
+  //   const navigate = useNavigate();
+
+  const dados = location.state as SimulacaoResponse | null;
+  if (!dados) {
+    return (
+      <section>
+        <p>Nenhuha simulação encontrada</p>
+        <a href="/">Voltar</a>
+      </section>
+    );
+  }
+  return (
+    <section>
+      {dados.planos.map((plano) => (
+        <div key={plano.meses}>
+          <h2>{plano.meses} meses</h2>
+          <p>Valor da parcela: R$ {plano.valorParcela.toFixed(2)}</p>
+          <p>Percentual do salário: {plano.percentualSalario.toFixed(2)}%</p>
+          <p>Juros mensal: {plano.jurosMensal.toFixed(2)}%</p>
+        </div>
+      ))}
+    </section>
+  );
+}
