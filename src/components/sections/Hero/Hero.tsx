277e7886b092a979a309ddcd9e4b82bb4c7f010f
Washington <washington.contato1327@gmail.com>
2026-03-03 12:45:48 -0300

    style:Inicia estilização com Tailwind CSS

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -30,32 +30,55 @@ export default function Hero() {
   }, [simulacao, navigate]);
 
   return (
-    <section>
-      <h1>Bem-vindo ao SimulAi</h1>
+    <section className="flex justify-evenly bg-[#f4f4f4] p-14">
+      <div className="">
+        <h1 className="text-6xl">
+          SimulAi <br /> Empréstimos
+        </h1>
+        <h2 className="mt-2 text-3xl">
+          As melhores opções, com as <br /> melhores condições de pagamento
+        </h2>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
+        <form
+          onSubmit={handleSubmit}
+          className="grid gap-3 mt-4 w-[450px] p-7 bg-white rounded-lg shadow"
+        >
+          <p className="text-xl">Simule seu empréstimo em segundos!</p>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            className="bg-[#f4f4f4] p-4 outline-none rounded"
+          />
+
+          <input
+            type="number"
+            placeholder="Salário Bruto"
+            value={salarioBruto}
+            onChange={(e) =>
+              setSalarioBruto(
+                e.target.value === "" ? "" : Number(e.target.value),
+              )
+            }
+            className="bg-[#f4f4f4] p-4 outline-none rounded"
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-[#9b00cf] p-4 rounded text-white"
+          >
+            {loading ? "Simulando..." : "Simular Empréstimo"}
+          </button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </form>
+      </div>
+      <div className="border w-[700px] rounded">
+        <img
+          src="https://res.cloudinary.com/dkrpmbjml/image/upload/v1772552420/emprestai_ssrjca.jpg"
+          className="w-full h-full"
         />
-
-        <input
-          type="number"
-          placeholder="Salário Bruto"
-          value={salarioBruto}
-          onChange={(e) =>
-            setSalarioBruto(e.target.value === "" ? "" : Number(e.target.value))
-          }
-        />
-
-        <button type="submit" disabled={loading}>
-          {loading ? "Simulando..." : "Simular Empréstimo"}
-        </button>
-
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </form>
+      </div>
     </section>
   );
 }
