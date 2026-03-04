433557bf7a6b4d7a6802a33bb6a075f3bb0aeb07
Washington <washington.contato1327@gmail.com>
2026-03-04 17:09:48 -0300

    feat(home): ajusta layout e estrutura da página inicial

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
 import Hero from "../../components/sections/Hero/Hero";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
   return (
     <main>
-      Hello
-      <button onClick={handleClick}>Count: {count}</button>
       <Hero />
     </main>
   );
