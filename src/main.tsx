f173b79e00ab26dd32ef40c85a794a36dd231483
Washington <washington.contato1327@gmail.com>
2026-03-04 17:09:43 -0300

    chore(app): ajusta estrutura base e estilos globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./assets/fonts/fonts.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/layout/Header/Header.tsx";
+import Footer from "./components/layout/Footer/Footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Header />
       <App />
+      <Footer />
     </BrowserRouter>
   </StrictMode>,
 );
