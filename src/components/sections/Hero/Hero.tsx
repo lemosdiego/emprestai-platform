40435f27dab58abc0e84108533021b12bfa320a7
Washington <washington.contato1327@gmail.com>
2026-03-04 17:09:56 -0300

    feat(hero): melhora layout e comportamento do Hero

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -2,13 +2,30 @@ import { useEffect, useState } from "react";
 import type { FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSimulacao } from "../../../hooks/useSimulacao";
+import "./Hero.css";
 
 export default function Hero() {
   const { simulacao, simularEmprestimo, loading, error } = useSimulacao();
   const navigate = useNavigate();
 
-  const [nome, setNome] = useState<string>("");
-  const [salarioBruto, setSalarioBruto] = useState<number | "">("");
+  const [nome, setNome] = useState("");
+  const [salarioBruto, setSalarioBruto] = useState<number | null>(null);
+  const [salarioBrutoFormatado, setSalarioBrutoFormatado] = useState("");
+
+  function maskMoneyBR(valor: string) {
+    const somenteNumeros = valor.replace(/\D/g, "");
+
+    if (!somenteNumeros) {
+      return { formatted: "", value: null };
+    }
+
+    const numero = Number(somenteNumeros);
+
+    return {
+      formatted: numero.toLocaleString("pt-BR"),
+      value: numero,
+    };
+  }
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -17,7 +34,7 @@ export default function Hero() {
 
     simularEmprestimo({
       nome,
-      salarioBruto: Number(salarioBruto),
+      salarioBruto, // 👈 número limpo
     });
   };
 
@@ -30,54 +47,55 @@ export default function Hero() {
   }, [simulacao, navigate]);
 
   return (
-    <section className="flex justify-evenly bg-[#f4f4f4] p-14">
-      <div className="">
-        <h1 className="text-6xl">
-          SimulAi <br /> Empréstimos
-        </h1>
-        <h2 className="mt-2 text-3xl">
-          As melhores opções, com as <br /> melhores condições de pagamento
-        </h2>
+    <section className="hero__section">
+      <div className="hero-section__overlay">
+        <div className="hero-section__container">
+          <div className="hero-section__text-container">
+            <h1>
+              EmprestAi! <br /> Crédito Simples. <br /> Sem enrrolação
+            </h1>
+            <p>
+              A emprestAi conecta você às melhores opções de crédito de forma
+              rápida, transparente e 100% digital.
+            </p>
+          </div>
+          <div className="hero-section__form-container">
+            <form onSubmit={handleSubmit}>
+              <p className="text-xl">Simule seu empréstimo em segundos!</p>
 
-        <form
-          onSubmit={handleSubmit}
-          className="grid gap-3 mt-4 w-[450px] p-7 bg-white rounded-lg shadow"
-        >
-          <p className="text-xl">Simule seu empréstimo em segundos!</p>
-          <input
-            type="text"
-            placeholder="Nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            className="bg-[#f4f4f4] p-4 outline-none rounded"
-          />
+              <input
+                type="text"
+                placeholder="Nome"
+                value={nome}
+                onChange={(e) => {
+                  const somenteLetras = e.target.value.replace(
+                    /[^a-zA-ZÀ-ÿ\s]/g,
+                    "",
+                  );
+                  setNome(somenteLetras);
+                }}
+              />
 
-          <input
-            type="number"
-            placeholder="Salário Bruto"
-            value={salarioBruto}
-            onChange={(e) =>
-              setSalarioBruto(
-                e.target.value === "" ? "" : Number(e.target.value),
-              )
-            }
-            className="bg-[#f4f4f4] p-4 outline-none rounded"
-          />
-          <button
-            type="submit"
-            disabled={loading}
-            className="bg-[#9b00cf] p-4 rounded text-white"
-          >
-            {loading ? "Simulando..." : "Simular Empréstimo"}
-          </button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-      </div>
-      <div className="border w-[700px] rounded">
-        <img
-          src="https://res.cloudinary.com/dkrpmbjml/image/upload/v1772552420/emprestai_ssrjca.jpg"
-          className="w-full h-full"
-        />
+              <input
+                type="text"
+                placeholder="Salário Bruto"
+                value={salarioBrutoFormatado}
+                onChange={(e) => {
+                  const { formatted, value } = maskMoneyBR(e.target.value);
+
+                  setSalarioBruto(value);
+                  setSalarioBrutoFormatado(formatted);
+                }}
+              />
+
+              <button type="submit" disabled={loading} className="">
+                {loading ? "Simulando..." : "Simular Empréstimo"}
+              </button>
+
+              {error && <p className="text-red-500">{error}</p>}
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
