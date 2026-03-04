e0e3982f97873e646aec309850157e9765565db5
Washington <washington.contato1327@gmail.com>
2026-03-04 17:09:53 -0300

    feat(layout): atualiza componente Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,33 +1,39 @@
 export default function Header() {
   return (
-    <header className="p-14 bg-[#9b00cf] flex items-center justify-around sticky top-0 z-10 drop-shadow-black ">
-      <a href="/">
-        <span className="text-4xl text-white">SimulAi</span>
-      </a>
-      <nav>
-        <ul className="flex gap-4 text-lg text-white">
-          <li>
-            <a
-              href="https://washington-app.netlify.app/portfolio"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:font-bold"
-            >
-              Meu Portfólio
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/lemosdiego/EmprestAi-spring-api.git"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:font-bold"
-            >
-              Repositório
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header className="p-4 bg-[#9b00cf] flex items-center justify-center sticky top-0 z-20 drop-shadow-black font-medium ">
+      <div className="flex items-center justify-between w-350 ">
+        <span className="rounded w-52 bg-white p-2">
+          <img
+            src="/logosf.png"
+            alt="SimulAi"
+            className="w-full h-full rounded"
+          />
+        </span>
+        <nav>
+          <ul className="flex gap-4 text-lg text-white">
+            <li>
+              <a
+                href="https://washington-app.netlify.app/portfolio"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-[#1A001F] transition-colors duration-500"
+              >
+                Meu Portfólio
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/lemosdiego/EmprestAi-spring-api.git"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-[#1A001F] transition-colors duration-500"
+              >
+                Repositório
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
