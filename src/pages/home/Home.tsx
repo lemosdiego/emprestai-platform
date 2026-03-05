fd51fa6c829401eaa62dce2510f7b6deba87d776
Washington <washington.contato1327@gmail.com>
2026-03-05 14:36:51 -0300

    feat(home): compõe a página inicial integrando todas as novas seções

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,17 @@
+import Benefits from "../../components/sections/Benefits/Benefits";
+import FAQ from "../../components/sections/FAQ/FAQ";
 import Hero from "../../components/sections/Hero/Hero";
+import HowItWorks from "../../components/sections/HowItWorks./HowItWorks";
+import SocialProof from "../../components/sections/SocialProof/SocialProof.";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <SocialProof />
+      <HowItWorks />
+      <Benefits />
+      <FAQ />
     </main>
   );
 }
