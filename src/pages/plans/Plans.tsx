c651b4f8fd9e1f07553621ff4b1a71c50f6f4894
Washington <washington.contato1327@gmail.com>
2026-03-06 16:38:28 -0300

    feat: Chama novo componente na página de planos

--- src/pages/plans/Plans.tsx
+++ src/pages/plans/Plans.tsx
@@ -1,13 +1,13 @@
 import { useLocation } from "react-router-dom";
-import Simulations from "../../components/sections/Simulations/Simulations";
 import FAQ from "../../components/sections/FAQ/FAQ";
+import ListPlans from "../../components/sections/ListPlans/ListPlans";
 
 export default function Plans() {
   const location = useLocation();
   console.log("Dados recebidos em Plans:", location.state);
   return (
     <main>
-      <Simulations />
+      <ListPlans />
       <FAQ />
     </main>
   );
